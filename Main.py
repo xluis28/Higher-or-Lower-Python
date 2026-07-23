a6295cfda929b9cddfdcd262ff24823489d6c3ea
Luis Victoria <luisvic02520@icloud.com>
2026-07-22 19:02:49 -0700

    Add application icon

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
 from PySide6.QtWidgets import QApplication
 from WelcomeWindow import WelcomeWindow
+from PySide6.QtGui import QIcon
 import sys
 
 app = QApplication(sys.argv)
+app.setWindowIcon(QIcon("Images/Arrows Icon.png"))
 window = WelcomeWindow()
 window.show()
 app.exec()
